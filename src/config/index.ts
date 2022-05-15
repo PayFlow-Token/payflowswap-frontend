340833a6cbee5ccbe89866ceaa7f50f3742e77cc
Hadrien <devnext21@gmail.com>
2022-05-15 21:08:05 +0700

    Update default Base URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import { ChainId } from '@pancakeswap/sdk'
 import BigNumber from 'bignumber.js/bignumber'
 import { BIG_TEN } from 'utils/bigNumber'
+import { APP_URL } from './constants/endpoints'
 
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
@@ -24,7 +25,7 @@ export const BASE_BSC_SCAN_URLS = {
 export const CAKE_PER_BLOCK = 40
 export const BLOCKS_PER_YEAR = (60 / BSC_BLOCK_TIME) * 60 * 24 * 365 // 10512000
 export const CAKE_PER_YEAR = CAKE_PER_BLOCK * BLOCKS_PER_YEAR
-export const BASE_URL = 'https://pancakeswap.finance'
+export const BASE_URL = APP_URL
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_URL}/add`
 export const BASE_BSC_SCAN_URL = BASE_BSC_SCAN_URLS[ChainId.MAINNET]
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
