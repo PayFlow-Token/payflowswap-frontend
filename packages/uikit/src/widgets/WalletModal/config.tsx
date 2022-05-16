f6bfd774503a6a3d0da24a522be54907d58bf9d0
Hadrien <devnext21@gmail.com>
2022-05-16 08:51:13 +0700

    Update metamask link

--- packages/uikit/src/widgets/WalletModal/config.tsx
+++ packages/uikit/src/widgets/WalletModal/config.tsx
@@ -17,7 +17,7 @@ const connectors: Config[] = [
     icon: Metamask,
     connectorId: ConnectorNames.Injected,
     priority: 1,
-    href: "https://metamask.app.link/dapp/pancakeswap.finance/",
+    href: "https://metamask.app.link/dapp/payflowswap.com",
   },
   {
     title: "Binance Wallet",
